58a3db7152d69b15086e8153b7d1d4d8cebbac4e
sumsol <xiecz0513@outlook.com>
2020-02-25 23:54:07 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    /**
+     * 查找数据库中贴子的内容，并全部返回
+     * @param userId 用户的id，用于查找“我的贴子”时使用，查找全部贴子时默认0
+     * @param offset 分页每一页起始行行号
+     * @param limit 每一页最多显示的数据数量
+     * @return 返回存储贴子的列表
+     */
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param注解用于给参数取别名，
+    // 如果只有一个参数，并且在<if>里使用，则必须加别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
