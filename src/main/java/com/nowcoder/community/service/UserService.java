c9639112e163750514549c024289a39519a0c114
sumsol <xiecz0513@outlook.com>
2020-03-11 20:39:49 +0800

    2020/3/11 commit

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -189,5 +189,10 @@ public class UserService implements CommunityConstant {
 
         return map;
     }
+
+    public User findUserByName(String username) {
+        return userMapper.selectByName(username);
+    }
+
 }
 
